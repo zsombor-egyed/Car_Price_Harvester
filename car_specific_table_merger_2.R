a7dc1b7c8907011d2d11d0b2e375039c9f436910
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2016-08-14 23:06:46 +0200

    Update car_specific_table_merger_2.R

--- car_specific_table_merger_2.R
+++ car_specific_table_merger_2.R
@@ -3,23 +3,23 @@ overall_property_list <- c()
 
 for (car in cars){
   print(car)
-  property_table <- tryCatch(read.csv(paste0("./binded_tables/", car), stringsAsFactors = FALSE), error = function(e){"NA"})
-  if (property_table != "Missing"){
-    overall_property_list <- unique(c(overall_property_list,property_table[,1]))}
+  property_table <- tryCatch(read.csv(paste0("./binded_tables/", car), stringsAsFactors = FALSE), error = function(e){"M"})
+  if (property_table != "M"){
+    overall_property_list <- unique(c(overall_property_list,property_table[,1]))
+  }
 }
 
 overall_property_list <- sort(overall_property_list)
 
-property_frame <- data.frame(matrix(0,length(cars),(length(overall_property_list)+1)))
-
+property_frame <- data.frame(matrix(0, length(cars), (length(overall_property_list) + 1)))
 colnames(property_frame) <- c("ID", overall_property_list)
 
 i <- 0
 for (car in cars){
   i <- i + 1
   print(i)
-  property_table <- tryCatch(read.csv(paste0("./binded_tables/", car), stringsAsFactors = FALSE), error = function(e) "Missing")
-  if (property_table != "Missing"){
+  property_table <- tryCatch(read.csv(paste0("./binded_tables/", car), stringsAsFactors = FALSE), error = function(e){"M"})
+  if (property_table != "M"){
     for (p in property_table[, 1]){
       property_frame[i, p] <- property_table[p == property_table[, 1], 2]
     }
