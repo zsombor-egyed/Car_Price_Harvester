2a2b611e90857d1c1f2ea13add5f8a70080299eb
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2016-08-14 23:05:13 +0200

    Update car_specific_table_merger.R

--- car_specific_table_merger.R
+++ car_specific_table_merger.R
@@ -1,9 +1,7 @@
 harvested_tables <- list.files("./car_specific_tables/")
 
 position <- unlist((lapply(strsplit(harvested_tables, ''), function(x) which(x == '_'))))
-
 page_origin <- as.numeric(substr(harvested_tables, 1, position - 1))
-
 indices <- unique(page_origin)
 
 for (i in indices){
@@ -11,9 +9,10 @@ for (i in indices){
   proper_tables <- harvested_tables[page_origin == i]
   flag  <- 0
   for (p in proper_tables){
-    input_table <- data.frame(tryCatch(read.csv(paste0("./car_specific_tables/", p), stringsAsFactors = FALSE), error = function(e){"M"}), stringsAsFactors = FALSE)
+    input_table <- tryCatch(read.csv(paste0("./car_specific_tables/", p), stringsAsFactors = FALSE), error = function(e){"M"})
+    input_table <- data.frame(input_table, stringsAsFactors = FALSE)
     if (nrow(input_table)>5){
-      if (flag ==0){
+      if (flag == 0){
         output_table <- input_table
         flag <- 1
       }
